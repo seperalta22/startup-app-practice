709395fe896f316c831d0a03d0ae48f375df076a
Sergio Peralta <seperalta88@gmail.com>
2023-10-31 23:10:07 -0300

    Add activity routes to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+
 Rails.application.routes.draw do
+  get 'activity/mine'
+  get 'activity/feed'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "activity#mine"
 end
