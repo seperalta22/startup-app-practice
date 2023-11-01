2599eef741aa0f94ef9de8452eac6d24389b6c47
Sergio Peralta <seperalta88@gmail.com>
2023-11-01 11:04:53 -0300

    Add Stimulus dropdown functionality and update import map

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stimulus-dropdown" # @2.1.0
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "hotkeys-js" # @3.12.0
+pin "stimulus-use" # @0.51.3
