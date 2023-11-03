c099b55d61bd1c4b0e99b027848d54858cb75e8e
Sergio Peralta <seperalta88@gmail.com>
2023-11-03 10:48:43 -0300

    Add Devise authentication to User model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 
 Rails.application.routes.draw do
+  devise_for :users
   get 'activity/mine'
   get 'activity/feed'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
