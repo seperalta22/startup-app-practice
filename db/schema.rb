a0276dfce0b6d22bc00f64e13bf4e1ad15abddf2
Sergio Peralta <seperalta88@gmail.com>
2023-11-03 11:40:48 -0300

    Configure RSpec for code coverage.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_11_03_134355) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
