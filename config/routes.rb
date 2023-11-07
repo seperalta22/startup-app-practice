22e76a49e4928adb039eda8bcef1664ce7b32e6a
Sergio Peralta <seperalta88@gmail.com>
2023-11-07 16:48:12 -0300

    Add name validation to Account model and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-
 Rails.application.routes.draw do
   devise_for :users
-  get 'activity/mine'
-  get 'activity/feed'
+  resource :account
+  get "activity/mine"
+  get "activity/feed"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
