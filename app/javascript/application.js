3948b0ffd2ad4006bbc9eea02adf775c5ab2882b
Sergio Peralta <seperalta88@gmail.com>
2023-11-07 15:14:31 -0300

    Add flash library to import map

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { Application } from "@hotwired/stimulus"
+import"lib/flash"
 import Dropdown from 'stimulus-dropdown'
 
 const application = Application.start()
